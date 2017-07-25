b8b3affeb9d70306fc4490e54a812b4d43a0cb45
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-07-25 20:27:32 +0200

    Added average model

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 from keras.models import Sequential
 from keras.layers import LSTM, Dropout, Dense, Activation, Input, Embedding
 from keras.optimizers import RMSprop
-
+from keras.models import load_model
+from keras.layers.merge import Average
+from keras.models import Model
+import os
 
 def M1_Embedding_128_256_relu(seq_length, num_inputs):
     model = Sequential()
@@ -87,16 +90,46 @@ def M7_128_256_relu_dropout(seq_length, num_inputs):
     model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
     return model
 
+def M8_M5_average_merge(models_folder):
+    truncated_models_outputs = []
+    models = []
+    for model_file in os.listdir(models_folder):
+        # Load single model
+        model_path = os.path.join(models_folder, model_file)
+        current_model = load_model(model_path)
+        models.append(current_model)
+
+        # Get rid of softmax activation layer so we get raw percentages
+        current_model.pop()
+        truncated_models_outputs.append(current_model.layers[-1].output)
+
+    new_models = []
+    input_layer = Input(shape=(SEQUENCE_LENGTH-1, len(labels)))
+    for model in models:
+        model = model(input_layer)
+        new_models.append(model)
+    
+    # Merge all truncated models and add softmax at the end
+    output = Average()([model for model in new_models])
+    output = Activation("softmax", name="predictions")(output)
+
+    model = Model(input_layer, output)
+    optimizer = RMSprop(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
+    return model    
+
+
 
 MODELS = {"M1": M1_Embedding_128_256_relu,
-		  "M2": M2_Embedding_32_64_relu,
-		  "M3": M3_Embedding_32_64_64_64_relu,
-		  "M4": M4_Embedding_256_512_relu,
-		  "M5": M5_128,
-		  "M6": M6_128_256_relu,
-		  "M7": M7_128_256_relu_dropout}
+          "M2": M2_Embedding_32_64_relu,
+          "M3": M3_Embedding_32_64_64_64_relu,
+          "M4": M4_Embedding_256_512_relu,
+          "M5": M5_128,
+          "M6": M6_128_256_relu,
+          "M7": M7_128_256_relu_dropout,
+          "M8": M8_M5_average_merge}
 
 
-def build_model(model_name, seq_length, num_inputs):
-	model = MODELS[model_name](seq_length, num_inputs)
-	return model
+def build_model(model_name, *args):
+    model = MODELS[model_name](*args)
+    return model
